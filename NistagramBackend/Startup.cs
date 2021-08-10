073e7646f2d6b1f3dffb4ea2ccc989f078b5ceb2
Dejan Jovanović <dejan.jovanovi94@gmail.com>
2021-08-10 22:23:31 +0200

    Close #1

--- NistagramBackend/Startup.cs
+++ NistagramBackend/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NistagramUtils.Offline.Post;
+using NistagramUtils.Offline.Post.Services;
 
 namespace NistagramBackend
 {
@@ -24,7 +26,7 @@ namespace NistagramBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IPostServices, PostServicesImpl>();
             services.AddControllers();
         }
 
