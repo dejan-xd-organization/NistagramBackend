f50eaa38b18f0eba9f27b0426c4dced1f2a5ea81
Dejan Jovanović <dejan.jovanovi94@gmail.com>
2021-08-16 00:10:27 +0200

    Close #3

---
+++ NistagramBackend/Services/Intefrace/ILoginService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NistagramBackend.Services.Intefrace
+{
+    interface ILoginService
+    {
+
+    }
+}
